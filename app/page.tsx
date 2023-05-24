9c5c10718c3189d6928885080ac4fe3a522fe0a6
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-05-24 11:17:35 +0530

    feat: :sparkles: add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
-  return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center">
-      <h1>Hello Everyone</h1>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen w-full flex-col items-center justify-center">
+			<h1>Hello Everyone</h1>
+		</main>
+	);
 }
