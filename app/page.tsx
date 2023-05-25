01a39e8e8499369d65d35442e02b7ee9b1305ef5
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-05-25 18:13:18 +0530

    feat: :sparkles: add hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Hero } from "@/components/landing-page";
+
 export default async function Home() {
 	return (
 		<main className="flex min-h-screen bg-secondary-dark w-full flex-col items-center justify-center">
-			<h1>Hello Everyone</h1>
+			<Hero />
 		</main>
 	);
 }
---
+++ components/landing-page/hero/hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+	return (
+		<div className="h-screen w-full flex flex-col justify-center items-center">
+			<h1 className="text-4xl text-primary-yellow font-quicksand tracking-widest text-center">
+				{"It's as simple as"}
+			</h1>
+		</div>
+	);
+};
+
+export default Hero;
