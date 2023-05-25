19379d49ae031d053060462c5d7b8583f007cc68
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-05-25 18:14:04 +0530

    feat: :sparkles: add hero component

--- components/landing-page/hero/hero.tsx
+++ components/landing-page/hero/hero.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
 	return (
 		<div className="h-screen w-full flex flex-col justify-center items-center">
 			<h1 className="text-4xl text-primary-yellow font-quicksand tracking-widest text-center">
-				{"It's as simple as"}
+				{"Content creation as simple as"}
 			</h1>
 		</div>
 	);
