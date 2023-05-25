b564b52ffc20090529f39672f4de6705b7160018
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-05-25 17:45:44 +0530

    feat: :sparkles: add base colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default async function Home() {
 	return (
-		<main className="flex min-h-screen w-full flex-col items-center justify-center">
+		<main className="flex min-h-screen bg-secondary-dark w-full flex-col items-center justify-center">
 			<h1>Hello Everyone</h1>
 		</main>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"secondary-dark": "#141923",
+				"dark-yellow": "#FFA02B",
+				"primary-yellow": "#F6BA3D",
+				"vermilion": "#FF5968",
+				"cobalt-blue": "#2E86C1",
+				"rose-pink": "#C71BDB",
+				"primary-orange": "#FC5830",
+			},
+		},
+	},
+	plugins: [],
+};
